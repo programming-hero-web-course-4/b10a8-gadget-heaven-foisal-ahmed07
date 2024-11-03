1101b3d5581fd0b4e67f836842c0c45949c606e4
Foisal Ahmed <shakhfoisal99@gmail.com>
2024-11-03 19:42:51 +0600

    footer section added

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
-        <div>
-            <h2>Footer</h2>
-        </div>
-    );
+  return (
+    <div>
+      <div className="space-y-3 border-b pb-8">
+        <h2 className="text-4xl font-bold text-center">Gadget Heaven</h2>
+        <p className=" font-semibold text-gray-500 text-center">
+          Leading the way in cutting-edge technology and innovation
+        </p>
+      </div>
+      <footer className="footer bg-white text-gray-500 font-semibold p-10">
+  <nav>
+    <h6 className="text-black text-xl font-bold">Services</h6>
+    <a className="link link-hover">Branding</a>
+    <a className="link link-hover">Design</a>
+    <a className="link link-hover">Marketing</a>
+    <a className="link link-hover">Advertisement</a>
+  </nav>
+  <nav>
+    <h6 className="text-black text-xl font-bold">Company</h6>
+    <a className="link link-hover">About us</a>
+    <a className="link link-hover">Contact</a>
+    <a className="link link-hover">Jobs</a>
+    <a className="link link-hover">Press kit</a>
+  </nav>
+  <nav>
+    <h6 className="text-black text-xl font-bold">Legal</h6>
+    <a className="link link-hover">Terms of use</a>
+    <a className="link link-hover">Privacy policy</a>
+    <a className="link link-hover">Cookie policy</a>
+  </nav>
+</footer>
+    </div>
+  );
 };
 
 export default Footer;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,10 +3,10 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="w-11/12 mx-auto bg-[#9538E2]">
+    <div className="w-11/12 mx-auto bg-[#9538E2] p-10 mt-10 rounded-3xl">
       <div className="flex justify-between items-center">
         <h2 className="text-xl font-bold text-white">Gadget Heaven</h2>
-        <div className="flex gap-5 items-center">
+        <div className="flex gap-5 items-center font-bold text-white ">
           <NavLink to="/">Home</NavLink>
           <NavLink to="/statistics">Statistics</NavLink>
           <NavLink to="/dashboard">DashBoard</NavLink>
